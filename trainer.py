f278b8d5d1cfcddbd13cbfabf4d232f3ffc768d9
Riddhiman Dasgupta <riddhiman.dasgupta@gmail.com>
2017-04-11 18:28:08 +0530

    Minor indexing fix

--- trainer.py
+++ trainer.py
@@ -18,7 +18,7 @@ class Trainer(object):
         self.optimizer.zero_grad()
         loss, k = 0.0, 0
         indices = torch.randperm(len(dataset))
-        for idx in tqdm(xrange(len(dataset)),desc='Training epoch '+str(self.epoch)+''):
+        for idx in tqdm(xrange(len(dataset)),desc='Training epoch '+str(self.epoch+1)+''):
             ltree,lsent,rtree,rsent,label = dataset[indices[idx]]
             linput, rinput = Var(lsent), Var(rsent)
             target = Var(map_label_to_target(label,dataset.num_classes))
